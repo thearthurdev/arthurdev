ca496d02e62759e33ad2010759e677e18c8cf994
thearthurdev <arthurdelords@gmail.com>
2021-03-16 22:52:27 +0000

    fixed section order

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -255,7 +255,7 @@ class LeftPanel extends StatelessWidget {
 
   Widget buildSection(BuildContext context) {
     switch (currentSection) {
-      case 3:
+      case 0:
         return StackClip(
           backgroundWidget: IntroSectionLeft(),
           foregroundWidget: PortfolioSectionLeft(),
@@ -278,7 +278,7 @@ class LeftPanel extends StatelessWidget {
               (kScreenHeight(context) * currentSection) -
               (kToolbarHeight * currentSection),
         );
-      case 0:
+      case 3:
         return JobSectionLeft();
         break;
       default:
