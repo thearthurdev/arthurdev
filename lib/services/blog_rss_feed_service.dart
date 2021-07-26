303d96ad1074c4468f3294413051c9c32188a6d3
thearthurdev <arthurdelords@gmail.com>
2021-07-26 13:54:30 +0000

    remove webfeed dependency

--- lib/services/blog_rss_feed_service.dart
+++ lib/services/blog_rss_feed_service.dart
@@ -1,39 +1,39 @@
-import 'package:http/http.dart' as http;
-import 'package:webfeed/webfeed.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:webfeed/webfeed.dart';
 
-class RssFeedService {
-  static RssFeed _feed;
+// class RssFeedService {
+//   static RssFeed _feed;
 
-  static get feed => _feed;
+//   static get feed => _feed;
 
-  static Future<RssFeed> _getFeed() async {
-    final _authority = "thearthur.dev";
-    final _path = "rss.xml";
-    final _feedUri = Uri.https(_authority, _path);
+//   static Future<RssFeed> _getFeed() async {
+//     final _authority = "thearthur.dev";
+//     final _path = "rss.xml";
+//     final _feedUri = Uri.https(_authority, _path);
 
-    print(_feedUri.toString());
+//     print(_feedUri.toString());
 
-    try {
-      final client = http.Client();
-      final response = await client.get(_feedUri);
+//     try {
+//       final client = http.Client();
+//       final response = await client.get(_feedUri);
 
-      print(response.body);
+//       print(response.body);
 
-      return RssFeed.parse(response.body);
-    } catch (e) {
-      print('error: $e');
-      return null;
-    }
-  }
+//       return RssFeed.parse(response.body);
+//     } catch (e) {
+//       print('error: $e');
+//       return null;
+//     }
+//   }
 
-  static Future loadFeed() async {
-    _getFeed().then((result) {
-      if (result == null || result.toString().isEmpty) {
-        return;
-      }
+//   static Future loadFeed() async {
+//     _getFeed().then((result) {
+//       if (result == null || result.toString().isEmpty) {
+//         return;
+//       }
 
-      _feed = result;
-      print('200');
-    });
-  }
-}
+//       _feed = result;
+//       print('200');
+//     });
+//   }
+// }
